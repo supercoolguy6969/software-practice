d1e685e0dbaaaae0e27ec9d2d5d6449083877051
Supercoolguy6969 <ankar3008@gmail.com>
2022-12-21 13:34:29 -0500

    .

--- Swift/SwiftUI_WeatherApp/SwiftUI_WeatherApp/ContentView.swift
+++ Swift/SwiftUI_WeatherApp/SwiftUI_WeatherApp/ContentView.swift
@@ -15,30 +15,72 @@ struct ContentView: View {
             LinearGradient(colors: [.blue,.white], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea(.all)
             
-            // City name
             VStack {
-                Text("Toronto, ON")
+                
+                // City name
+                Text("Toronto, CA")
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
                 
+                // Image
                 Image(systemName: "cloud.sun.fill")
                     .renderingMode(.original)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 180, height: 180)
                 
+                // Temperature
+                Text("70°")
+                    .font(.system(size: 50, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                
                 Spacer()
                 
+                HStack {
+                    DayWeatherView(city: "MON", image: "cloud.fill", temp: 70)
+                    
+                    
+                }
             }
-
+            
         }
-        
     }
-}
-
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        }
     }
+    
+struct DayWeatherView: View {
+        
+var city: String
+var image: String
+var temp: Int
+        
+var body: some View {
+    VStack {
+        // City name
+        Text(city)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+        
+        // Image
+        Image(systemName: image)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 180, height: 180)
+        
+        // Temperature
+        Text("\(temp)°")
+            .font(.system(size: 50, weight: .medium, design: .default))
+            .foregroundColor(.white)
+        
+        
+    }
+  }
+ }
 }
