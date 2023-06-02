a1bb9cd972c086ef656233689b8f459b9ecbca82
Ankar Kieram <ankarkieram@Ankars-MacBook-Pro.local>
2023-06-02 10:28:55 -0400

    .

--- C:C++/pointers.c
+++ C:C++/pointers.c
@@ -3,9 +3,18 @@
 
 int main()
 {
+    
+    // INTRODUCTION
+    // Allocation of memory:   int - 4 bytes allocated, char - 1 byte allocated, float - 4 byes allocated
+    
+    
+    int a = 10;     // this will allocate 4 bytes of memory in the RAM (random address)
+    
+    // Pointers - variables that store address of another variable. So here p - address and *p - value at address Eg:
 
-    int a = 10;
     int *p;
-    p = &a;
-    printf("Value of a is %d", *p + 1);
+    p = &a;  // Store the address of 'a' using the '&' operator to pointer variable p
+    printf("print value of p: %d, which is the address of a: %d",*p, p);
+
+    printf("Value of a is %d", *p);   // with *p, it will print the value of the address pointed by p 
 }
